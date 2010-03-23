07fb717d9be005c44b432dc676243c2c087f25d5
unknown <Shalom@.(none)>
2010-03-23 13:51:46 -0700

    Fix the script not to include the selection in the cmd.input. This include causes for the ruby process to halt, as it takes the needed data from the ENV.

--- commands/convert_html_to_haml.rb
+++ commands/convert_html_to_haml.rb
@@ -2,7 +2,7 @@ require 'ruble'
 
 command 'Convert HTML to HAML' do |cmd|
   cmd.output = :replace_selection
-  cmd.input = :selection
+  cmd.input = :none
   cmd.invoke =<<-EOF
 ruby -e "require 'rubygems'; gem 'haml'; require 'haml/html'; puts Haml::HTML.new(ENV['TM_SELECTED_TEXT']).render"
 EOF
