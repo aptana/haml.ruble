534e65e5a699f059d6287a96ad199741300f3f80
sgtcoolguy <cwilliams@aptana.com>
2010-04-20 11:17:14 -0400

    add key binding

--- commands/convert_html_to_haml.rb
+++ commands/convert_html_to_haml.rb
@@ -1,8 +1,9 @@
 require 'ruble'
 
 command 'Convert HTML to HAML' do |cmd|
+  cmd.key_binding = 'OPTION+COMMAND+c'
   cmd.output = :replace_selection
-  cmd.input = :none
+  cmd.input = :selection
   cmd.invoke =<<-EOF
 ruby -e "require 'rubygems'; gem 'haml'; require 'haml/html'; puts Haml::HTML.new(ENV['TM_SELECTED_TEXT']).render"
 EOF
