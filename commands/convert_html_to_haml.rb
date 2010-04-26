219b3b4ac40b86946148b7e54945bc5b3f51822a
sgtcoolguy <cwilliams@aptana.com>
2010-04-26 17:08:50 -0400

    fix some keybindings to work around current bugs/limitations

--- commands/convert_html_to_haml.rb
+++ commands/convert_html_to_haml.rb
@@ -1,7 +1,7 @@
 require 'ruble'
 
 command 'Convert HTML to HAML' do |cmd|
-  cmd.key_binding = 'OPTION+COMMAND+c'
+  cmd.key_binding = 'OPTION+COMMAND+C'
   cmd.output = :replace_selection
   cmd.input = :selection
   cmd.invoke =<<-EOF
