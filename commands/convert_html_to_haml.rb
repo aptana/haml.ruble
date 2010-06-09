ab165710518227bd554fe3d70e3de7579e07e25c
Allen Yeung <ayeung@aptana.com>
2010-06-08 17:43:12 -0700

    Fix for bug #555 (HTML to HAML command)

--- commands/convert_html_to_haml.rb
+++ commands/convert_html_to_haml.rb
@@ -4,7 +4,12 @@ command 'Convert HTML to HAML' do |cmd|
   cmd.key_binding = 'OPTION+COMMAND+C'
   cmd.output = :replace_selection
   cmd.input = :selection
-  cmd.invoke =<<-EOF
-ruby -e "require 'rubygems'; gem 'haml'; require 'haml/html'; puts Haml::HTML.new(ENV['TM_SELECTED_TEXT']).render"
-EOF
+  cmd.invoke do |context|
+    cmd_line = "ruby -e \"require 'rubygems'; gem 'haml'; require 'haml/html'; puts Haml::HTML.new(STDIN.read).render\""
+    IO.popen(cmd_line, 'r+') do |io|
+      io << ENV['TM_SELECTED_TEXT']
+      io.close_write
+      io.read
+    end
+  end
 end
