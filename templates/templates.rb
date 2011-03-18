8064c78e8986eb5ac9881044b6f4ae5f222f553b
Allen Yeung <ayeung@aptana.com>
2011-03-18 13:55:02 -0700

    Fix #1740 HAML file template causes an error

--- templates/templates.rb
+++ templates/templates.rb
@@ -4,7 +4,8 @@ template "HAML Template" do |t|
   t.filetype = "*.haml"
   t.invoke do |context|
     ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.haml")
+    #we remove /lib since we don't have a lib directory in the haml ruble and it fails when we try to navigate from there
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}".chomp("/lib") + "/templates/template.haml")
     raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
  end
 end
